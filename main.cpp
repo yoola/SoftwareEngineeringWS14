1ae22d4eb0236d96df6440cd95deb8b76027530b
Johannes <johanneshartmann90@gmx.de>
2014-10-20 20:48:37 +0200

    Converter-Commit
    +Converter .h and .cpp
    +dollarToEuro .h and .cpp
    +main.h

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "dollartoeuroconverter.h"
+
+int main()
+{
+    dollarToEuroConverter* myConverter = new dollarToEuroConverter();
+    double aLotOfDollars = 10000;
+    double aLotOfEuros = myConverter->convert(aLotOfDollars);
+    std::cout << myConverter->toString() << " has converted "<< aLotOfDollars << " Dollar to " << aLotOfEuros <<" Euros!"<<std::endl;
+    return 0;
+}
+
