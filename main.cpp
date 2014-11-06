3fb133ac26fb32e42766b9d225e305d94b5acd03
Jula McGibbon <julamcgibbon@gmail.com>
2014-11-06 01:39:26 +0100

    Rewrote main function using the factory

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
-#include "dollartoeuroconverter.hpp"
+#include <string>
+#include <iostream>
+#include <sstream>
+#include "converter.hpp"
 
 int main(int argc, char* argv[])
 {
-  std::string conversion = argv[0];
-  std::string value = argv[1];
+    std::string conversion = argv[1];
+    std::string value_string = argv[2];
 
-  /*
-   * TODO
-   *
-   * use desired conversion here
-   *
-  */
+    // Convert string to double
+    std::istringstream value_stream(value_string);
+    double value;
+    value_stream >> value;
 
-  dollarToEuroConverter* myConverter = new dollarToEuroConverter();
-  double aLotOfDollars = 10000;
-  double aLotOfEuros = myConverter->convert(aLotOfDollars);
-  std::cout << myConverter->toString() << " has converted "<< aLotOfDollars << " Dollar to " << aLotOfEuros <<" Euros!"<<std::endl;
-  return 0;
+    /*
+     * use desired conversion here
+     *
+     */
+    Factory factory;
+    converter* myConverter = factory.create(conversion);
+
+    double converted_value = myConverter->convert(value);
+
+    std::cout << myConverter->toString() << " has converted " << value << " to " << converted_value << "!" << std::endl;
+    return 0;
 }
-
